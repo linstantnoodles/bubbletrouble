39b1209aa722158c61c2c16ade5ca0c998049c72
Alan <linpd2@gmail.com>
2013-07-14 01:30:19 -0400

    added node server and socket

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-var app = require('express').createServer();
-var io = require('socket.io').listen(app);
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
 
-app.listen(80);
+app.listen(5000);
 
-app.get('/', function(req, res) {
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
 
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
