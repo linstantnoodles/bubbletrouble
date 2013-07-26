7e1564c601a43c4295c2146a87e6c62d0fe86cfb
Alan <linpd2@gmail.com>
2013-07-25 22:22:40 -0400

    added socket counting

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ var app = require('http').createServer(handler)
 
 app.listen(5000);
 
+// This will get incremented every second
+var mainValue = 0;
+function logIt() {
+    console.log(mainValue);
+    mainValue++;
+}
+function increaseByOne() {
+    setInterval(logIt, 1000);
+}
+
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
   function (err, data) {
@@ -17,9 +27,20 @@ function handler (req, res) {
   });
 }
 
+// call the timer
+increaseByOne();
+
 io.sockets.on('connection', function (socket) {
+  // start listening to events
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
   });
+
+  socket.on('getCount', function(data) {
+      socket.emit('outputCount', {time: mainValue});
+    console.log("Nigga tryna get mah count!");
+  });
+
 });
+
