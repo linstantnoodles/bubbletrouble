6790a6add960b62f239d1ff42861e47659fb4bee
Alan <linpd2@gmail.com>
2014-02-08 16:18:29 -0500

    use express and added dependency file

--- app.js
+++ app.js
@@ -1,51 +1,21 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
+var express = require('express')
+  , app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server)
   , fs = require('fs')
   , url = require('url')
   , Game = require('./server/game').Game;
 
-app.listen(5000);
+app.use(express.static(__dirname + '/client'));
 // reduce logging
 io.set('log level', 1);
-
 // Game map
 var games = {};
-
-// Request handler
-var assetDirectory = './client';
-var headerMap = {
-    'jpg': 'image/jpg',
-    'png': 'image/png',
-    'wav': 'audio/x-wav',
-    'mp3': 'audio/mpeg',
-    'css': 'text/css',
-}
-
-function handler (req, res) {
-  var request = url.parse(req.url, true);
-  var pathToFile = request.pathname;
-  var urlParts = pathToFile.split('/');
-  var content = (urlParts[urlParts.length-1]).split('.');
-  // If static file
-  if (content.length > 1) {
-    var extension = content[content.length-1];
-    var data = fs.readFileSync(assetDirectory + pathToFile);
-    res.writeHead(200, {'Content-Type': headerMap[extension]});
-    res.end(data);
-  }
-  // Send index
-  if (pathToFile == '/') {
-    var data = fs.readFileSync('./client/index.html');
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(data);
-  }
-}
-
 // Main socket
 io.sockets.on('connection', function (socket) {
-  // start listening to events
+  // Start listening to events
   socket.emit('acknowledge');
-
   socket.on('newGame', function(data) {
     var newGame = new Game();
     newGame.start();
@@ -65,3 +35,7 @@ io.sockets.on('connection', function (socket) {
   });
 
 });
+
+var port = Number(process.env.PORT || 5000);
+server.listen(port);
+
